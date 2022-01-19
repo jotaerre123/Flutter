89ba11dd01013ee48117b17158861cfde154d943
Jaime <jaimejimenezrios10@gmail.com>
2022-01-19 17:03:36 +0100

    listas con tabs y drawer

---
+++ flutter_listas_starwars2 tabs/lib/main.dart
@@ -0,0 +1,226 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'model/planets.dart';
+import 'model/starwars.dart';
+
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<People>> items;
+  late Future<List<Planetas>> itemsPlanets;
+
+  @override
+  void initState() {
+    items = fetchPeople();
+    itemsPlanets = fetchPlanets();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: "Characters"),
+                Tab(text: 'Planets',)
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Center(
+                child: FutureBuilder<List<People>>(
+                  future: items,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return _peopleList(snapshot.data!);
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+          
+                    return const Center(child: CircularProgressIndicator());
+                  },
+                ),
+              ),
+              Center(
+                child: FutureBuilder<List<Planetas>>(
+                  future: itemsPlanets,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return _planetsList(snapshot.data!);
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+          
+                    return const Center(child: CircularProgressIndicator());
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<List<People>> fetchPeople() async {
+    final response = await http.get(Uri.parse('https://swapi.dev/api/people'));
+    if (response.statusCode == 200) {
+      return StarWars.fromJson(jsonDecode(response.body)).results;
+    } else {
+      throw Exception('Failed to load people');
+    }
+  }
+
+  Widget _peopleList(List<People> peopleList) {
+    return SizedBox(
+      height: 250,
+      width: MediaQuery.of(context).size.width,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: peopleList.length,
+        itemBuilder: (context, index) {
+          return _peopleItem(peopleList.elementAt(index), index);
+        },
+      ),
+    );
+  }
+
+  Widget _peopleItem(People people, int index) {
+    return Container(
+        margin: const EdgeInsets.symmetric(vertical: 20.0),
+        width: 150,
+        child: Card(
+          child: InkWell(
+            splashColor: Colors.red.withAlpha(30),
+            onTap: () {
+              debugPrint('Card tapped.');
+            },
+            child: SizedBox(
+              width: 300,
+              height: 150,
+              child: Column(
+                children: [
+                  Text(people.name),
+                  Image.network(
+                    'https://starwars-visualguide.com/assets/img/characters/' +
+                        (index + 1).toString() +
+                        '.jpg',
+                    width: 100,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+
+  Widget _getPlanetImg(int index, String name){
+    if (name == 'Tatooine') {
+      return Image.network('https://static.wikia.nocookie.net/esstarwars/images/b/b0/Tatooine_TPM.png/revision/latest?cb=20131214162357');
+    } else {
+      return Image.network('https://starwars-visualguide.com/assets/img/planets/'+(index+1).toString()+'.jpg', width: 100);
+    }
+  }
+
+  Future<List<Planetas>> fetchPlanets() async {
+    final response = await http.get(Uri.parse('https://swapi.dev/api/planets'));
+    if (response.statusCode == 200) {
+      return StarPlanets.fromJson(jsonDecode(response.body)).results;
+    } else {
+      throw Exception('Failed to load planets');
+    }
+  }
+
+  Widget _planetsList(List<Planetas> planetsList) {
+    return SizedBox(
+      height: 250,
+      width: MediaQuery.of(context).size.width,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: planetsList.length,
+        itemBuilder: (context, index) {
+          return _planetItem(planetsList.elementAt(index), index);
+        },
+      ),
+    );
+  }
+
+  Widget _planetItem(Planetas planet, int index) {
+    return Container(
+        margin: const EdgeInsets.symmetric(vertical: 20.0),
+        width: 150,
+        child: Card(
+          child: InkWell(
+            splashColor: Colors.red.withAlpha(30),
+            onTap: () {
+              debugPrint('Card tapped.');
+            },
+            child: SizedBox(
+              width: 300,
+              height: 150,
+              child: Column(
+                children: [
+                  Text(planet.name),
+                  _getPlanetImg(index, planet.name)
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
